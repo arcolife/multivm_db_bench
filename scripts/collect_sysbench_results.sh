e22271c2628d206d78fdeb6bf8e286aa5b88f96b
Archit Sharma <arcsharm@redhat.com>
2016-03-11 12:21:54 +0530

    add aio mode check to collect script

--- scripts/collect_sysbench_results.sh
+++ scripts/collect_sysbench_results.sh
@@ -2,6 +2,11 @@
 
 AIO_MODE=$1
 
+if [[ ! $AIO_MODE =~ ^(native|threads)$ ]]; then
+  echo "wrong aio mode supplied. choose (native|threads)";
+  exit 1
+fi
+
 for i in `cat /tmp/vm_hostnames`; do
     echo "collecting results file on: $i"
     ssh root@$i "grep transac /root/scripts/results/327*$AIO_MODE*txt" > /tmp/"$i"_"$AIO_MODE"_transactions.txt
