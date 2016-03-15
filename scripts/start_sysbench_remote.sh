caa0a7927db7833057c2ee144ec95476e35bf542
Archit Sharma <arcsharm@redhat.com>
2016-03-15 15:24:45 +0530

    fix sysbench start script call in userbenchmark

--- scripts/start_sysbench_remote.sh
+++ scripts/start_sysbench_remote.sh
@@ -10,6 +10,6 @@ trap user_interrupt SIGTSTP
 
 for machine in `cat /tmp/vm_hostnames`; do
   echo "starting sysbench test (pbench enabled) for $AIO_MODE.."
-  ssh root@$machine 'source /etc/multivm.config && ${MULTIVM_ROOT_DIR%/}/start_sysbench_remote.sh'
+  ssh root@$machine 'source /etc/multivm.config && ${MULTIVM_ROOT_DIR%/}/start_sysbench.sh'
   # ssh root@$machine 'source /etc/multivm.config; ${MULTIVM_ROOT_DIR%/}/run-sysbench-pbench.sh >> ${RESULTS_DIR%/}/$AIO.txt 2>&1 &'
 done
